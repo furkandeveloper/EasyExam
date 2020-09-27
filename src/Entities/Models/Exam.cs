303edd431309fbc66a1eeed0ced1c634c6412d44
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-09-27 22:07:53 +0300

    Add exam model

---
+++ src/Entities/Models/Exam.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Models
+{
+    /// <summary>
+    /// Exam entity
+    /// </summary>
+    public class Exam : BaseModel
+    {
+        /// <summary>
+        /// Exam title
+        /// </summary>
+        public string Title { get; set; }
+        
+        /// <summary>
+        /// Exam Start Date
+        /// </summary>
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Exam End date
+        /// </summary>
+        public DateTime EndDate { get; set; } = DateTime.UtcNow.AddHours(1);
+
+        // Note: To be Added question model using reflection.
+    }
+}
