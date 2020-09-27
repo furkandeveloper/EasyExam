447c4211b46d7afefd727bc02f5796ba6da15df7
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-09-27 15:25:53 +0300

    Add documentation tool

---
+++ samples/WebApi/Docs/README.md
@@ -0,0 +1,15 @@
+﻿<p align="center">
+  <img src="https://user-images.githubusercontent.com/47147484/94364171-19bdd180-00d0-11eb-91e1-dffb5d120013.png" />
+</p>
+
+[![CodeFactor](https://www.codefactor.io/repository/github/furkandeveloper/easyexam/badge)](https://www.codefactor.io/repository/github/furkandeveloper/easyexam)
+[![CircleCI](https://circleci.com/gh/furkandeveloper/EasyExam.svg?style=svg)](https://circleci.com/gh/circleci/circleci-docs)
+# Easy Exam Manager For .Net
+
+Heeey, This library aims to simplify the exam space for developers.
+Follow me;
+  - Turn on your computer.
+  - Prepare your coffee.
+  - And sit back.
+  
+  To be continued...
--- samples/WebApi/Startup.cs
+++ samples/WebApi/Startup.cs
@@ -1,6 +1,8 @@
+using MarkdownDocumenting.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,11 +14,19 @@ namespace WebApi
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +36,7 @@ namespace WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseDocumentation(opts => this.Configuration.Bind("DocumentationOptions", opts));
 
             app.UseRouting();
 
