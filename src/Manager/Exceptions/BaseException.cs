6cca90b8ffc25a5354919484a33f3bce9ccafeb9
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-09-30 23:42:05 +0300

    :art: Add base exception

---
+++ src/Manager/Exceptions/BaseException.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Exceptions
+{
+    /// <summary>
+    /// Base Exception
+    /// </summary>
+    public class BaseException : Exception
+    {
+        /// <summary>
+        /// Without paramater ctor.
+        /// </summary>
+        public BaseException():base()
+        {
+
+        }
+
+        /// <summary>
+        /// Exception with key.
+        /// </summary>
+        /// <param name="key">
+        /// Exception key.
+        /// </param>
+        public BaseException(string key) : base(key)
+        {
+
+        }
+
+        /// <summary>
+        /// Exception with key and innerException.
+        /// </summary>
+        /// <param name="key">
+        /// Exception key.
+        /// </param>
+        /// <param name="innerException">
+        /// Exception.
+        /// </param>
+        public BaseException(string key, Exception innerException) : base(key,innerException)
+        {
+
+        }
+    }
+}
