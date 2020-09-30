dacc5c8e32b314f196141ce5798c0a7aab96f2c5
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-09-30 20:58:58 +0300

    :rocket: Add interface for exam

---
+++ src/Manager/Repositories/Abstractions/IExamRepository.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Repositories.Abstractions
+{
+    /// <summary>
+    /// This interface includes custom queries for exam entity.
+    /// </summary>
+    public interface IExamRepository : IBaseRepository<Exam>
+    {
+
+    }
+}
