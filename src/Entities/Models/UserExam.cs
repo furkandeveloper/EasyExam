7ad648a3db30bb5768c753e601f8b775d837eae4
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-01 21:49:25 +0300

    :hammer: Add UserExam entity. This class merges user and exam.

---
+++ src/Entities/Models/UserExam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Models
+{
+    /// <summary>
+    /// This class merges user and exam.
+    /// </summary>
+    public class UserExam
+    {
+        /// <summary>
+        /// Question document id.
+        /// </summary>
+        public string QuestionId { get; set; }
+
+        /// <summary>
+        /// Answer of question.
+        /// </summary>
+        public string Answer { get; set; }
+
+        /// <summary>
+        /// Note of answer.
+        /// </summary>
+        public string AnswerNote { get; set; }
+    }
+}
