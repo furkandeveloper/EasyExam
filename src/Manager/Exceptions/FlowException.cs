fc2ed2c2a9efb35d98cab45ca7eda638358356b3
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-01 22:13:41 +0300

    :hammer: Add Flow Exception class.

---
+++ src/Manager/Exceptions/FlowException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Exceptions
+{
+    /// <summary>
+    /// Flow exception class.
+    /// For example;
+    /// ```csharp
+    /// var exam = new Exam()
+    /// {
+    ///     Users = new List<User>()   <---- throw new FlowException(nameof(Users));
+    /// }
+    /// ```
+    /// </summary>
+    public class FlowException : BaseException
+    {
+        /// <summary>
+        /// Without parameter ctor.
+        /// </summary>
+        public FlowException()
+        {
+        }
+
+        /// <summary>
+        /// Exception with key.
+        /// </summary>
+        /// <param name="key">
+        /// Exception key.
+        /// </param>
+        public FlowException(string key) : base(key)
+        {
+        }
+        /// <summary>
+        /// Exception with key and innerException.
+        /// </summary>
+        /// <param name="key">
+        /// Exception key.
+        /// </param>
+        /// <param name="innerException">
+        /// Exception.
+        /// </param>
+        public FlowException(string key, Exception innerException) : base(key, innerException)
+        {
+        }
+    }
+}
