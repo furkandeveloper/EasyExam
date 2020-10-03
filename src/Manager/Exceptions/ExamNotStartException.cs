00d83e7819ba987e4eb3986a3568d98960f0a6d1
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-03 22:54:58 +0300

    :hammer: Add exam not start exception class.

---
+++ src/Manager/Exceptions/ExamNotStartException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Exceptions
+{
+    /// <summary>
+    /// Exception class. Entry is not allowed before the exam starts.
+    /// </summary>
+    public class ExamNotStartException : BaseException
+    {
+        /// <summary>
+        /// Without parameter.
+        /// </summary>
+        public ExamNotStartException()
+        {
+        }
+
+        /// <summary>
+        /// Exception with key.
+        /// </summary>
+        /// <param name="key">
+        /// Exception key.
+        /// </param>
+        public ExamNotStartException(string key) : base(key)
+        {
+        }
+        /// <summary>
+        /// Exception with key and innerException.
+        /// </summary>
+        /// <param name="key">
+        /// Exception key.
+        /// </param>
+        /// <param name="innerException">
+        /// Exception.
+        /// </param>
+        public ExamNotStartException(string key, Exception innerException) : base(key, innerException)
+        {
+        }
+    }
+}
