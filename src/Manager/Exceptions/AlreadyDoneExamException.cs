5de2c034dcb3b3cf41c16758a5f23bece48cece3
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-03 22:55:10 +0300

    :hammer: Add already done exam exception class.

---
+++ src/Manager/Exceptions/AlreadyDoneExamException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Exceptions
+{
+    /// <summary>
+    /// Already done exam exception class.
+    /// </summary>
+    public class AlreadyDoneExamException : BaseException
+    {
+        /// <summary>
+        /// Without parameter.
+        /// </summary>
+        public AlreadyDoneExamException()
+        {
+        }
+
+        /// <summary>
+        /// Exception with key.
+        /// </summary>
+        /// <param name="key">
+        /// Exception key.
+        /// </param>
+        public AlreadyDoneExamException(string key) : base(key)
+        {
+        }
+        /// <summary>
+        /// Exception with key and innerException.
+        /// </summary>
+        /// <param name="key">
+        /// Exception key.
+        /// </param>
+        /// <param name="innerException">
+        /// Exception.
+        /// </param>
+        public AlreadyDoneExamException(string key, Exception innerException) : base(key, innerException)
+        {
+        }
+    }
+}
