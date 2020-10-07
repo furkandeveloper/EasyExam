6a59496f3b4a13b406f2281cdf2f60babcb6c3cf
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-07 22:42:15 +0300

    :memo: Add xml comment for mongo setting.

--- src/Manager/Configuration/MongoSetting.cs
+++ src/Manager/Configuration/MongoSetting.cs
@@ -4,10 +4,19 @@ using System.Text;
 
 namespace Manager.Configuration
 {
+    /// <summary>
+    /// Database setting.
+    /// </summary>
     public class MongoSetting
     {
+        /// <summary>
+        /// Connection string.
+        /// </summary>
         public string ConnectionString { get; set; }
 
+        /// <summary>
+        /// Database name.
+        /// </summary>
         public string Database { get; set; }
     }
 }
