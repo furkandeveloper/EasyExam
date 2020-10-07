b472083fe413e82fa6395eab0f5b0d0286abdc6f
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-07 22:18:22 +0300

    :hammer: Add detail exam result dto.

---
+++ src/Manager/Models/DetailExamResult.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Models;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Models
+{
+    public class DetailExamResult : SummaryExamResult
+    {
+        public List<ExamQuestion> ExamQuestions { get; set; }
+    }
+
+    public class ExamQuestion : Question
+    {
+        public QuestionAnswer QuestionAnswer { get; set; }
+    }
+
+    public class QuestionAnswer : UserExam
+    {
+        public bool IsCorrect { get; set; }
+    }
+
+}
