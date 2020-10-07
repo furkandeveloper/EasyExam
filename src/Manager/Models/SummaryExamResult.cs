7f913e5848e6aafc0ef7f990f305838bda3262f2
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-07 22:18:01 +0300

    :hammer: Add summary response dto

---
+++ src/Manager/Models/SummaryExamResult.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Models
+{
+    public class SummaryExamResult
+    {
+        public User User { get; set; }
+
+        public int TotalQuestion { get; set; }
+
+        public int TotalCorrect { get; set; }
+
+        public int TotalWrong { get; set; }
+    }
+}
